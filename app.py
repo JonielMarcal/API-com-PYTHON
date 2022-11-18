669444722397ed8379044b80c0d647b9ffb29bc9
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-18 15:46:52 -0300

    metodos a mais criados! PUT, POST e DELETE

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ livros = [
     },
 ]
 
-# consultar todos os livros
+# consultar todos os livro
 
 
 @app.route('/livros', methods=['GET'])
@@ -40,6 +40,34 @@ def obter_livro_id(id):
     for livro in livros:
         if livro.get('id') == id:
             return jsonify(livro)
+
+# editar o livro, recebendo ID
+
+
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+# add um novo livro
+
+
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros)
+
+
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+    return jsonify(livros)
 
 
 app.run(port=5000, host='localhost', debug=True)
