7de90ec1ed6197627c889063070168d9841f32b5
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-18 09:48:06 -0300

    metodos GET busca por ID

--- app.py
+++ app.py
@@ -18,4 +18,28 @@ livros = [
         'titulo': 'A Vingança Sombria ',
         'autor': 'Jean Marcos Tomas'
     },
+    {
+        'id': 4,
+        'titulo': 'O Rei da Bola',
+        'autor': 'Reytler Matheus'
+    },
 ]
+
+# consultar todos os livros
+
+
+@app.route('/livros', methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+# consultar por ID
+
+
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+
+app.run(port=5000, host='localhost', debug=True)
